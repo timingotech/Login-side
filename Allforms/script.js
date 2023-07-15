421301f52a7824e320e9d457fcdf053515048adc
timingotech <113597643+timingotech@users.noreply.github.com>
2023-07-16 00:36:12 +0100

    Add files via upload

---
+++ Allforms/script.js
@@ -0,0 +1,14 @@
+const passwordInput = document.getElementById('password');
+const showPasswordIcon = document.getElementById('showPasswordIcon');
+const hidePasswordIcon = document.getElementById('hidePasswordIcon');
+
+showPasswordIcon.addEventListener('click', function() {
+  passwordInput.type = 'text';
+  showPasswordIcon.style.display = 'none';
+  hidePasswordIcon.style.display = 'inline-block';
+});
+hidePasswordIcon.addEventListener('click', function() {
+    passwordInput.type = 'password';
+    hidePasswordIcon.style.display = 'none';
+    showPasswordIcon.style.display = 'inline-block';
+  });
